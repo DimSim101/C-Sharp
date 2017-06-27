1d5c72a99b31295853f525db5453b61ea5858017
David Aaron <dsaaron123@gmail.com>
2017-06-27 22:04:22 +1000

    Updated a little and also put in some nicer prints for end statement using Console.WriteLine arguments instead of newlines.

--- AboutMe/AboutMe/Program.cs
+++ AboutMe/AboutMe/Program.cs
@@ -15,13 +15,16 @@ namespace AboutMe
                 "See https://github.com/DimSim101/C-Sharp for more info." + "\n\n" +
                 "First tutorial course is C# Fundamentals by Bob Tabor - https://mva.microsoft.com/en-us/training-courses/c-fundamentals-for-absolute-beginners-16169?l=83b9cRQIC_9706218949 \n";
             string developerInfo = "Project Developer: David Aaron" + "\n\n" + "Project Start Date: June 2017" +
-                "\n\n" +"Head over to https://kingdavid.xyz for more info about me :) \n";
+                "\n\n" +"Head over to my website: https://kingdavid.xyz for more info about me :) \n";
+
+            string contactMe = "Check out the rest of the repository! Feel free to contact me (see Option 2 for details) with any projects you think I may be interested in :D.";
+
+            string goodbyeInstruction = "Hit the <Enter> key or Ctrl-C to exit...";
 
             string message = (userOptionChoice == "2") ? developerInfo : generalInfo;
 
             Console.WriteLine("\n" + message);
-            Console.WriteLine("Check out the rest of the repository! Feel free to contact me (see Option 2 for details) with any projects you think I may be interested in :D." +
-                "\n\n" + "Hit the <Enter> key or Ctrl-C to exit...");
+            Console.WriteLine("{0}\n\n{1}", contactMe, goodbyeInstruction);
             Console.ReadLine();
         }
     }
