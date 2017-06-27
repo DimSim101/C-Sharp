4f239620bdcaf4e1d735558927cc94b2f93ba63e
David Aaron <dsaaron123@gmail.com>
2017-06-27 21:56:47 +1000

    First commit after changing namespace / project name to AboutMe and making some other changes. Was easier to just reset repo and start again since it was so early on in the dev phase.

---
+++ AboutMe/AboutMe/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AboutMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# World - Welcome to the About Me program!");
+            Console.WriteLine("Select an option for more info: 1 - [General Info] <DEFAULT> OR 2 - [Developer Info].");
+            string userOptionChoice = Console.ReadLine();
+
+
+            string generalInfo = "This is a repo for any C# tutorials I complete / any C# content I build." + "\n" +
+                "See https://github.com/DimSim101/C-Sharp for more info." + "\n\n" +
+                "First tutorial course is C# Fundamentals by Bob Tabor - https://mva.microsoft.com/en-us/training-courses/c-fundamentals-for-absolute-beginners-16169?l=83b9cRQIC_9706218949 \n";
+            string developerInfo = "Project Developer: David Aaron" + "\n\n" + "Project Start Date: June 2017" +
+                "\n\n" +"Head over to https://kingdavid.xyz for more info about me :) \n";
+
+            string message = (userOptionChoice == "2") ? developerInfo : generalInfo;
+
+            Console.WriteLine("\n" + message);
+            Console.WriteLine("Check out the rest of the repository! Feel free to contact me (see Option 2 for details) with any projects you think I may be interested in :D." +
+                "\n\n" + "Hit the <Enter> key or Ctrl-C to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# C# Repo for all my C# related material:
+
+# Run the AboutMe program for more info about me ... :)
+
+- Starting with Fundamentals of C# on MSDN: https://mva.microsoft.com/en-us/training-courses/c-fundamentals-for-absolute-beginners-16169
+
+- Will move onto web / mobile stuff after :)
