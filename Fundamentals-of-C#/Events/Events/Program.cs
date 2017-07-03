c194e2ef249f11b11c0306ad8c2f149c8a8d2962
David Aaron <dsaaron123@gmail.com>
2017-07-03 18:10:18 +1000

    Added basic events and event handling using a timer example. Also added WPF - Windows Presentation Framework click event which affects the button content and a labels content as well. Also had to manage delegating the ui updating from outside the timer thread, so that was nice to get it all working. Boom dreams done!

---
+++ Fundamentals-of-C#/Events/Events/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numSeconds = 1 * 1000;
+            Timer timer = new Timer(numSeconds);
+
+            timer.Elapsed += TimerElapsed;
+            timer.Elapsed += TimerElapsed2;
+
+            timer.Start();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hit <Enter> to remove the yellow event or Ctrl-C to exit.");
+            Console.ReadLine();
+            
+
+            timer.Elapsed -= TimerElapsed2;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hit <Enter> or Ctrl-C to exit.");
+            Console.ReadLine();
+            
+        }
+
+        private static void TimerElapsed(object sender, ElapsedEventArgs args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Time Elapsed: {0:HH:mm:ss.ffff}", args.SignalTime);
+        }
+
+        private static void TimerElapsed2(object sender, ElapsedEventArgs args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Time Elapsed2: {0:HH:mm:ss.ffff}", args.SignalTime);
+        }
+    }
+}
---
+++ Fundamentals-of-C#/WPFAppEvents/WPFAppEvents/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFAppEvents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Timer timer = new Timer(1000);
+
+        int count = 0;
+        string buttonContent = "Click Me";
+        string labelContent = "Click the button ... I dare you :)";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Elapsed += updateClickTimer;
+
+            clickMeButton.Click += startClicktimer;
+            clickMeButton.Click += myOwnClickFunc;
+        }
+
+
+        private void clickMeButtonClicked(object sender, RoutedEventArgs e)
+        {
+            if (!timer.Enabled)
+            {
+                clickMeLabel.Content = "HEY! I didn't think you'd actually click it :(";
+                clickMeButton.Content = "I've been clicked!";
+                Console.WriteLine("UI Updated");
+            }
+            
+        }
+
+        private void myOwnClickFunc(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("BUTTON CLICKED");
+        }
+
+        private void startClicktimer(object sender, RoutedEventArgs e)
+        {
+            if (!timer.Enabled)
+            {
+                timer.Start();
+            }
+        }
+
+        private void updateClickTimer(object sender, ElapsedEventArgs e)
+        {
+            count++;
+            if (count == 3)
+            {
+                count = 0;
+                // This delegates to work to the UI thread since we are updating UI elements here on another thread.
+                App.Current.Dispatcher.Invoke(delegate
+                {
+                    updateTextAndReenable();
+                });
+
+                timer.Stop();
+            }
+        }
+
+        private void updateTextAndReenable()
+        {
+            clickMeButton.Content = buttonContent;
+            clickMeLabel.Content = labelContent;
+            Console.WriteLine("UI Updated");
+        }
+    }
+}
